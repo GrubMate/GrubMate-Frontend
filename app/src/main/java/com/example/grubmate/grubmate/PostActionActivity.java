f7b4ac0ac04e6efbea9fe99ca43a7bf3ccd68cc7
Pengxiang Zhu <pengxiaz@usc.edu>
2017-10-08 21:32:05 -0700

    MapForPost

--- app/src/main/java/com/example/grubmate/grubmate/PostActionActivity.java
+++ app/src/main/java/com/example/grubmate/grubmate/PostActionActivity.java
@@ -1,13 +1,19 @@
 package com.example.grubmate.grubmate;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,10 +22,18 @@ import android.widget.Toast;
 import com.example.grubmate.grubmate.utilities.GrubMatePreference;
 import com.example.grubmate.grubmate.utilities.JsonUtilities;
 import com.example.grubmate.grubmate.utilities.NetworkUtilities;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 import java.io.IOException;
 
-public class PostActionActivity extends AppCompatActivity implements View.OnClickListener {
+public class PostActionActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
     private EditText postItemNameText;
     private EditText postItemTagsText;
     private EditText postItemDescriptionText;
@@ -27,10 +41,18 @@ public class PostActionActivity extends AppCompatActivity implements View.OnClic
     private EditText postItemAllergyText;
     private Spinner postItemCategorySpinner;
     private Spinner postItemTimeSpinner;
+    private Button postItemLocation;
+    private TextView postItemLocationText;
+    private GoogleApiClient mGoogleApiClient;
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    private double Lat;
+    private double Lng;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_post_action);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -42,12 +64,39 @@ public class PostActionActivity extends AppCompatActivity implements View.OnClic
         postItemDescriptionText = (EditText) findViewById(R.id.et_post_item_description);
         postItemAllergyText = (EditText) findViewById(R.id.et_post_item_allergy);
         postItemQuantityText = (EditText) findViewById(R.id.et_post_item_quantity);
-
+        postItemLocation = (Button) findViewById(R.id.et_location);
         postItemCategorySpinner = (Spinner) findViewById(R.id.spinner_category);
         ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(this,
                 R.array.food_category, android.R.layout.simple_spinner_item);
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         postItemCategorySpinner.setAdapter(categoryAdapter);
+        postItemLocationText = (TextView) findViewById(R.id.et_location_text) ;
+        //Auto_complete activity open when button clicked
+        postItemLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGoogleApiClient = new GoogleApiClient
+                .Builder(PostActionActivity.this)
+                        .addApi(Places.GEO_DATA_API)
+                        .addApi(Places.PLACE_DETECTION_API)
+                        .enableAutoManage(PostActionActivity.this, PostActionActivity.this)
+                        .build();
+
+                try {
+                    Intent intent =
+                            new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                                    .build(PostActionActivity.this);
+                    startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException e) {
+                    // TODO: Handle the error.
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // TODO: Handle the error.
+                }
+            }
+        });
+
+
+
 
 
         postItemTimeSpinner = (Spinner) findViewById(R.id.spinner_time);
@@ -60,6 +109,27 @@ public class PostActionActivity extends AppCompatActivity implements View.OnClic
         postItemTimeSpinner.setAdapter(timeAdapter);
     }
 
+    //Store the location's Latitude to Lat and Logitude to Lng
+    //Show location'name in the TextView of PostActionActivity
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+
+                Lat = place.getLatLng().latitude;
+                Lng = place.getLatLng().longitude;
+               postItemLocationText.setText(place.getName().toString());
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.i("test", status.getStatusMessage());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
     public void showShortToast(String msg) {
        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
@@ -83,6 +153,11 @@ public class PostActionActivity extends AppCompatActivity implements View.OnClic
             } else {
             showShortToast("Please fill out all necessary fields before posting");
         }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     public class PostActionTask extends AsyncTask<String, Integer, String> {
@@ -113,6 +188,9 @@ public class PostActionActivity extends AppCompatActivity implements View.OnClic
             }
         }
     }
+
+
+
 }
 
 /*
