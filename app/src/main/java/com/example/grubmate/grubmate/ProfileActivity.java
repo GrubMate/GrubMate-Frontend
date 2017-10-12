be107c0f87c91041688453fbba63a1318b9f8e90
Tianhang Liu <tianhanl@usc.edu>
2017-10-11 23:30:48 -0700

    setting up the profile activity

--- app/src/main/java/com/example/grubmate/grubmate/ProfileActivity.java
+++ app/src/main/java/com/example/grubmate/grubmate/ProfileActivity.java
@@ -2,12 +2,27 @@ package com.example.grubmate.grubmate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
+    ImageView mProfileAvatar;
+    TextView mProfileName;
+    EditText mProfileDescription;
+    RatingBar mProfileRatingBar;
+    int userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        mProfileAvatar = (ImageView) findViewById(R.id.i_profile_avatar);
+        mProfileName = (TextView) findViewById(R.id.tv_profile_name);
+        mProfileDescription = (EditText) findViewById(R.id.et_profile_description);
+        mProfileRatingBar = (RatingBar) findViewById(R.id.rb_profile);
     }
 }
