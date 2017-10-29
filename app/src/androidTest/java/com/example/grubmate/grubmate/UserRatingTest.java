acf864beed8cdb22c2e3f5bd13750ebe00019451
Tianhang Liu <tianhanl@usc.edu>
2017-10-28 19:50:18 -0700

    add test for notification center

---
+++ app/src/androidTest/java/com/example/grubmate/grubmate/UserRatingTest.java
@@ -0,0 +1,17 @@
+package com.example.grubmate.grubmate;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by tianhangliu on 10/28/17.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class UserRatingTest {
+    @Rule
+
+}
