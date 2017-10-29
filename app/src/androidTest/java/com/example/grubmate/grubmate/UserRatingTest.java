4cc44b2e66e73064b1f198d1ea89799f5c310bed
Tianhang Liu <tianhanl@usc.edu>
2017-10-28 19:57:59 -0700

    setting up test for rating

--- app/src/androidTest/java/com/example/grubmate/grubmate/UserRatingTest.java
+++ app/src/androidTest/java/com/example/grubmate/grubmate/UserRatingTest.java
@@ -1,10 +1,18 @@
 package com.example.grubmate.grubmate;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.grubmate.grubmate.fragments.NotificationCenterFragment;
+
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToHolder;
 
 /**
  * Created by tianhangliu on 10/28/17.
@@ -13,5 +21,16 @@ import org.junit.runner.RunWith;
 @LargeTest
 public class UserRatingTest {
     @Rule
+    public FragmentTestRule<NotificationCenterFragment> mFragmentTestRule = new FragmentTestRule<>(NotificationCenterFragment.class);
+
+    @Before
+    public void setUp() {
+        mFragmentTestRule.launchActivity(null);
+    }
+
+    @Test
+    public void has_rating_box() {
+        onView(ViewMatchers.withId(R.id.rv_notification_list));
+    }
 
 }
