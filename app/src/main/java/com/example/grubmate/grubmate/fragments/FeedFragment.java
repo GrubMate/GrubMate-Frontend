72c7f5dfce1e635172207480bfb0aa1d6091a32e
Pengxiang Zhu <pengxiaz@usc.edu>
2017-11-11 17:22:11 -0800

    map and quit toast

--- app/src/main/java/com/example/grubmate/grubmate/fragments/FeedFragment.java
+++ app/src/main/java/com/example/grubmate/grubmate/fragments/FeedFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,11 +17,11 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.example.grubmate.grubmate.MapFeedActivity;
 import com.example.grubmate.grubmate.MapsActivity;
 import com.example.grubmate.grubmate.R;
 import com.example.grubmate.grubmate.activities.ProfileActivity;
 import com.example.grubmate.grubmate.adapters.BFeedAdapter;
-import com.example.grubmate.grubmate.adapters.FeedAdapter;
 import com.example.grubmate.grubmate.dataClass.MockData;
 import com.example.grubmate.grubmate.dataClass.Post;
 import com.example.grubmate.grubmate.dataClass.UserRequest;
@@ -44,7 +45,8 @@ import java.util.Objects;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
-import static com.example.grubmate.grubmate.adapters.FeedAdapter.FeedDetailActivity.PLACE_AUTOCOMPLETE_REQUEST_CODE;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +63,7 @@ public class FeedFragment extends Fragment implements GoogleApiClient.OnConnecti
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -81,7 +84,7 @@ public class FeedFragment extends Fragment implements GoogleApiClient.OnConnecti
     private double Lat;
     private double Lng;
     private ArrayList<Post> mPastPostList;
-
+    private FloatingActionButton mapListButton;
     public FeedFragment() {
         // Required empty public constructor
     }
@@ -124,7 +127,12 @@ public class FeedFragment extends Fragment implements GoogleApiClient.OnConnecti
         View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
         // Setting up feed
         mFeedView = (RecyclerView) rootView.findViewById(R.id.rv_feed);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL, false);
+        LinearLayoutManager layoutManager;
+        if(mParam1 == "horizontal"){
+             layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        }else {
+             layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        }
         mFeedView.setLayoutManager(layoutManager);
 
         mFeedAdapter = new BFeedAdapter(feedData);
@@ -153,6 +161,15 @@ public class FeedFragment extends Fragment implements GoogleApiClient.OnConnecti
                         break;
                     default:
                 }
+            }
+        });
+        mapListButton = (FloatingActionButton) rootView.findViewById(R.id.fab_feed_map);
+        mapListButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getContext(),MapFeedActivity.class);
+                intent.putExtra("postList", feedData);
+                startActivity(intent);
             }
         });
         mFeedView.setAdapter(mFeedAdapter);
---
+++ app/src/main/java/com/example/grubmate/grubmate/fragments/MapFragment.java
@@ -0,0 +1,174 @@
+package com.example.grubmate.grubmate.fragments;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.grubmate.grubmate.R;
+import com.example.grubmate.grubmate.dataClass.Post;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MapFragment.OnMapFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MapFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private GoogleMap mMap;
+    private Marker myMarker;
+    private UiSettings mUiSettings;
+    private OnMapFragmentInteractionListener mListener;
+    private ArrayList<Post> postFeed;
+    private ArrayList<Marker> markers;
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MapFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MapFragment newInstance(String param1, String param2,ArrayList<Post> postList) {
+        MapFragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        args.putSerializable("postList",postList);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+            postFeed = (ArrayList<Post>) getArguments().getSerializable("postList");
+        }
+        markers = new ArrayList<>();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment)
+                        getChildFragmentManager().findFragmentById(R.id.map_feed);
+        mapFragment.getMapAsync(this);
+
+
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnMapFragmentInteractionListener) {
+            mListener = (OnMapFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+
+        mUiSettings = mMap.getUiSettings();
+        // Add a marker in Sydney and move the camera
+        mMap.setOnMarkerClickListener(this);
+        for(int i =0;i<postFeed.size();i++){
+            Post post = postFeed.get(i);
+            LatLng location = new LatLng(post.address[0], post.address[1]);
+            markers.add(mMap.addMarker(new MarkerOptions().position(location).title(post.title).snippet(post.description)));
+        }
+
+        LatLng firstPost = new LatLng(postFeed.get(0).address[0], postFeed.get(0).address[1]);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(firstPost));
+        mUiSettings.setZoomControlsEnabled(true);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(firstPost, 17.0f));
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        for(int i = 0; i<markers.size();i++){
+            if (marker.equals(markers.get(i)))
+            {
+                Toast.makeText(getContext(), "click success on item" + marker.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnMapFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
