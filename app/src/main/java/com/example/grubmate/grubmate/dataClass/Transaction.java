e9caa91345dbc967f551028dadd3f38c7eec8c05
Tianhang Liu <tianhanl@usc.edu>
2017-11-13 21:12:53 -0800

    setting up transaction list

---
+++ app/src/main/java/com/example/grubmate/grubmate/dataClass/Transaction.java
@@ -0,0 +1,8 @@
+package com.example.grubmate.grubmate.dataClass;
+
+/**
+ * Created by tianhangliu on 11/13/17.
+ */
+
+public class Transaction {
+}
---
+++ app/src/main/java/com/example/grubmate/grubmate/fragments/TransactionFragment.java
@@ -0,0 +1,113 @@
+package com.example.grubmate.grubmate.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.grubmate.grubmate.R;
+import com.example.grubmate.grubmate.adapters.BTransactionAdapter;
+import com.example.grubmate.grubmate.dataClass.Transaction;
+
+import java.util.ArrayList;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnTransactionInteractionListener}
+ * interface.
+ */
+public class TransactionFragment extends Fragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+    private OnTransactionInteractionListener mListener;
+    private BTransactionAdapter mTransactionAdapter;
+    private ArrayList<Transaction> transactionData;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public TransactionFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static TransactionFragment newInstance(int columnCount) {
+        TransactionFragment fragment = new TransactionFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+        transactionData = new ArrayList<>();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_transaction_list, container, false);
+        mTransactionAdapter = new BTransactionAdapter(transactionData);
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(mTransactionAdapter);
+        }
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnTransactionInteractionListener) {
+            mListener = (OnTransactionInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnTransactionInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnTransactionInteractionListener {
+        // TODO: Update argument type and name
+        void onTransactionInteraction();
+    }
+}
