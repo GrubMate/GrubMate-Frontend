9a12a30b4159463db5654cd026cd9ec84403d890
Tianhang Liu <tianhanl@usc.edu>
2017-11-17 16:23:51 -0800

    add rating

--- app/src/main/java/com/example/grubmate/grubmate/fragments/TransactionFragment.java
+++ app/src/main/java/com/example/grubmate/grubmate/fragments/TransactionFragment.java
@@ -1,22 +1,31 @@
 package com.example.grubmate.grubmate.fragments;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseQuickAdapter.OnItemChildClickListener;
 import com.example.grubmate.grubmate.R;
 import com.example.grubmate.grubmate.adapters.BTransactionAdapter;
+import com.example.grubmate.grubmate.dataClass.Notification;
 import com.example.grubmate.grubmate.dataClass.Transaction;
+import com.example.grubmate.grubmate.utilities.GrubMatePreference;
+import com.example.grubmate.grubmate.utilities.NetworkUtilities;
+import com.example.grubmate.grubmate.utilities.PersistantDataManager;
+import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -34,6 +43,9 @@ public class TransactionFragment extends Fragment {
     private OnTransactionInteractionListener mListener;
     private BTransactionAdapter mTransactionAdapter;
     private ArrayList<Transaction> transactionData;
+    private int userID;
+    private Gson gson;
+    private Context context;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -60,6 +72,9 @@ public class TransactionFragment extends Fragment {
             mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
         }
         transactionData = new ArrayList<>();
+        userID = PersistantDataManager.getUserID();
+        gson = new Gson();
+        context = getContext();
     }
 
     @Override
@@ -88,7 +103,7 @@ public class TransactionFragment extends Fragment {
                             view.setEnabled(false);
                             RatingBar ratingBar = (RatingBar) adapter.getViewByPosition(recyclerView, position,R.id.rb_transaction_rating);
                             int score = ratingBar.getNumStars();
-
+                            new SubmitRatingTask().execute(position, score);
                             break;
                     }
                 }
@@ -128,5 +143,44 @@ public class TransactionFragment extends Fragment {
     public interface OnTransactionInteractionListener {
         // TODO: Update argument type and name
         void onTransactionInteraction();
+    }
+    public class SubmitRatingTask extends AsyncTask<Integer, Integer, String> {
+        int pos;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Integer... params) {
+            Log.d("Rating", "Rating Sent");
+            pos = params[0];
+            Integer score = params[1];
+            Notification notification = new Notification();
+            int requesterID = transactionData.get(pos).requesterID;
+            int posterID = transactionData.get(pos).posterID;
+            notification.toUserID = posterID==userID?requesterID:posterID;
+            notification.fromUserID = posterID==userID?posterID:requesterID;
+            notification.rating = score;
+            notification.type = Notification.RATING;
+            try {
+                return NetworkUtilities.post(GrubMatePreference.getRatingUrl(notification.fromUserID, notification.toUserID, notification.rating), gson.toJson(notification));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String postActionResponse) {
+            Log.d("Rating Response", postActionResponse==null?"null":postActionResponse);
+            if (postActionResponse != null) {
+                Toast.makeText(context, "You successfully submit a rating", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Error: Network Error", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
