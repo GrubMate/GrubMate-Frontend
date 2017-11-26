c1e05449a3fce05e2afa00d6675a34966909d795
Pengxiang Zhu <pengxiaz@usc.edu>
2017-11-25 20:12:44 -0800

    splash screen timer

--- app/src/main/java/com/example/grubmate/grubmate/SplashActivity.java
+++ app/src/main/java/com/example/grubmate/grubmate/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.grubmate.grubmate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by PETER on 11/5/17.
@@ -13,9 +16,10 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(1));
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
+
     }
 }
